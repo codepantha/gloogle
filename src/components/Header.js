29a00e2763d49eb26ea0ebf8bf5422ce6e068d06
codepanther <pezechigozirim@gmail.com>
2022-01-23 08:52:37 +0100

    Improve the styling on Header and Results components

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,16 +7,24 @@ const Header = () => {
   return (
     <section className='header__container w-100 flex flex-column pa3 mb4'>
       <div className='header__container-topmenu flex justify-between items-center mb3'>
-        <AiOutlineMenu size={24} />
-        <p className="f3 lh-copy">Gloogle</p>
+        <AiOutlineMenu size={24} color='#041C32' />
+        <p className="f3 f2-l lh-copy" style={{ color: '#041C32'}}>
+          <span className='blue'>G</span>
+          <span className='dark-red'>l</span>
+          <span className='gold'>o</span>
+          <span className='light-blue'>o</span>
+          <span className='green'>g</span>
+          <span className='red'>l</span>
+          <span className='blue'>e</span>
+        </p>
         <div className="ba-none br4 pa2 bg-gray white">Hi</div>
       </div>
       <Search />
-      <nav className="w-100 flex justify-between items-end mt1">
-        <Link to='/search' className="gray link link:active pointer bb"><p className='f5'>Search</p></Link>
-        <Link to='/images' className="gray link link:active pointer"><p className='f5'>Images</p></Link>
-        <Link to='/news' className="gray link link:active pointer"><p className='f5'>News</p></Link>
-        <Link to='/videos' className="gray link link:active pointer"><p className='f5'>Videos</p></Link>
+      <nav className="w-100 flex justify-between justify-center-l gap-4 items-end mt1">
+        <Link to='/search' className="gray link link:active pointer bb mh3-l"><p className='f5'>Search</p></Link>
+        <Link to='/images' className="gray link link:active pointer mh3-l"><p className='f5'>Images</p></Link>
+        <Link to='/news' className="gray link link:active pointer mh3-l"><p className='f5'>News</p></Link>
+        <Link to='/videos' className="gray link link:active pointer mh3-l"><p className='f5'>Videos</p></Link>
       </nav>
     </section>
   )
--- src/components/Results.js
+++ src/components/Results.js
@@ -2,35 +2,40 @@ import React from 'react';
 
 const Results = () => {
   return (
-    <div className="w-100 bg-near-white min-vh-100">
+    <section className="w-100 bg-near-white min-vh-100 pt1 pt2-l">
       <div className="w-100 w-70-m w-70-l">
         <div className="w-100 bg-white mb2 pa3">
           <a href="https://semver.org" className="link dim black-60">https://semver.org</a>
-          <h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2>
-          <p>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
+          <a href="https://semver.org" style={{'color': '#325288', textDecoration: 'none'}}><h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2></a>
+          <p style={{ color: '#041C32' }}>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
         </div>
         <div className="w-100 bg-white mb2 pa3">
           <a href="https://semver.org" className="link dim black-60">https://semver.org</a>
-          <h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2>
-          <p>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
+          <a href="https://semver.org" style={{'color': '#325288', textDecoration: 'none'}}><h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2></a>
+          <p style={{ color: '#041C32' }}>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
         </div>
         <div className="w-100 bg-white mb2 pa3">
           <a href="https://semver.org" className="link dim black-60">https://semver.org</a>
-          <h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2>
-          <p>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
+          <a href="https://semver.org" style={{'color': '#325288', textDecoration: 'none'}}><h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2></a>
+          <p style={{ color: '#041C32' }}>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
         </div>
         <div className="w-100 bg-white mb2 pa3">
           <a href="https://semver.org" className="link dim black-60">https://semver.org</a>
-          <h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2>
-          <p>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
+          <a href="https://semver.org" style={{'color': '#325288', textDecoration: 'none'}}><h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2></a>
+          <p style={{ color: '#041C32' }}>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
         </div>
         <div className="w-100 bg-white mb2 pa3">
           <a href="https://semver.org" className="link dim black-60">https://semver.org</a>
-          <h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2>
-          <p>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
+          <a href="https://semver.org" style={{'color': '#325288', textDecoration: 'none'}}><h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2></a>
+          <p style={{ color: '#041C32' }}>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
+        </div>
+        <div className="w-100 bg-white mb2 pa3">
+          <a href="https://semver.org" className="link dim black-60">https://semver.org</a>
+          <a href="https://semver.org" style={{'color': '#325288', textDecoration: 'none'}}><h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2></a>
+          <p style={{ color: '#041C32' }}>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
         </div>
       </div>
-    </div>
+    </section>
   )
 };
 
