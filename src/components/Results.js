727bb452aca2cec502e4e016b87d10575e9a4f33
codepanther <pezechigozirim@gmail.com>
2022-01-23 07:32:52 +0100

    Style the Results.js component

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
 const Results = () => {
-  return <div className="w-100 bg-near-white vh-100">Results</div>;
+  return (
+    <div className="w-100 bg-near-white min-vh-100">
+      <div className="w-100 w-70-m w-70-l">
+        <div className="w-100 bg-white mb2 pa3">
+          <a href="https://semver.org" className="link dim black-60">https://semver.org</a>
+          <h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2>
+          <p>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
+        </div>
+        <div className="w-100 bg-white mb2 pa3">
+          <a href="https://semver.org" className="link dim black-60">https://semver.org</a>
+          <h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2>
+          <p>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
+        </div>
+        <div className="w-100 bg-white mb2 pa3">
+          <a href="https://semver.org" className="link dim black-60">https://semver.org</a>
+          <h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2>
+          <p>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
+        </div>
+        <div className="w-100 bg-white mb2 pa3">
+          <a href="https://semver.org" className="link dim black-60">https://semver.org</a>
+          <h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2>
+          <p>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
+        </div>
+        <div className="w-100 bg-white mb2 pa3">
+          <a href="https://semver.org" className="link dim black-60">https://semver.org</a>
+          <h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2>
+          <p>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Results;
