3e32245c04018b0601e97cc278b71cf6a563dd2f
codepanther <pezechigozirim@gmail.com>
2022-01-27 12:20:05 +0100

    Add margin before the footer

--- src/components/Results.js
+++ src/components/Results.js
@@ -65,38 +65,13 @@ const Results = () => {
                 </div>
               );
             })}
-            {/* <div className="w-100 bg-white mb2 pa3">
-              <a href="https://semver.org" className="link dim black-60">https://semver.org</a>
-              <a href="https://semver.org" style={{'color': '#325288', textDecoration: 'none'}}><h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2></a>
-              <p style={{ color: '#041C32' }}>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
-            </div>
-            <div className="w-100 bg-white mb2 pa3">
-              <a href="https://semver.org" className="link dim black-60">https://semver.org</a>
-              <a href="https://semver.org" style={{'color': '#325288', textDecoration: 'none'}}><h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2></a>
-              <p style={{ color: '#041C32' }}>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
-            </div>
-            <div className="w-100 bg-white mb2 pa3">
-              <a href="https://semver.org" className="link dim black-60">https://semver.org</a>
-              <a href="https://semver.org" style={{'color': '#325288', textDecoration: 'none'}}><h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2></a>
-              <p style={{ color: '#041C32' }}>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
-            </div>
-            <div className="w-100 bg-white mb2 pa3">
-              <a href="https://semver.org" className="link dim black-60">https://semver.org</a>
-              <a href="https://semver.org" style={{'color': '#325288', textDecoration: 'none'}}><h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2></a>
-              <p style={{ color: '#041C32' }}>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
-            </div>
-            <div className="w-100 bg-white mb2 pa3">
-              <a href="https://semver.org" className="link dim black-60">https://semver.org</a>
-              <a href="https://semver.org" style={{'color': '#325288', textDecoration: 'none'}}><h2>Semantic Versioning 2.0.0 | Semantic Versioning</h2></a>
-              <p style={{ color: '#041C32' }}>Software versioning is the process of assigning either unique version names or unique version numbers to unique states of computer software.</p>
-            </div> */}
           </div>
         </section>
       );
 
     case "/images":
       return (
-        <section className="w-100 bg-near-white min-vh-100 pt1 pt2-l">
+        <section className="w-100 bg-near-white pt1 pt2-l" style={{minHeight: '1000px', overflowY: 'scroll'}}>
           <div className="images__result-grid">
             {results.length
               ? results.map((result) => (
